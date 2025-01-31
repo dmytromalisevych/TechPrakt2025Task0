0d44b36648afc9e03732ff291fca511c8da3b5de
dmytromalisevych <146988176+dmytromalisevych@users.noreply.github.com>
2025-01-31 20:18:08 +0200

    Додано програму для обрахування

---
+++ task/task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+        Console.WriteLine($"Сума {num1} + {num2} = {sum}");
+
+        Console.ReadLine();
+    }
+}
